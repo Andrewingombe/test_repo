f86a2e02d73557b2c36e3e5b7355b999add07746
Andrew Ingombe <ingombe.ani@gmail.com>
2021-09-23 12:30:24 +0200

    added a get route to server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const { ppid } = require('process');
 const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello form the home route');
+})
 
 app.listen(3000, () => console.log(`listening on port 3000`));
